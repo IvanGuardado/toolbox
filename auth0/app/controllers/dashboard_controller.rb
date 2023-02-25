1cc88544006df42050a556b3b8275a3e8a159cb8
Iván Guardado Castro <dev.ivangc@gmail.com>
2023-02-26 00:30:14 +0100

    Configures M2M client

--- auth0/app/controllers/dashboard_controller.rb
+++ auth0/app/controllers/dashboard_controller.rb
@@ -1,7 +1,18 @@
+require 'auth0'
+
 class DashboardController < ApplicationController
     include Secured
 
     def show
         @user = session[:userinfo]
+        Rails.logger.info(auth0_client.user(session[:userinfo]["sub"]))
+    end
+
+    private def auth0_client
+        Auth0Client.new(
+            client_id: ENV['AUTH0_M2M_CLIENT_ID'],
+            client_secret: ENV['AUTH0_M2M_CLIENT_SECRET'],
+            domain: ENV['AUTH0_DOMAIN']
+        )
     end
 end
